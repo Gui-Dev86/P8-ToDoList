db8ef882b60e820d36382ff19b13bbe626ed4d07
Guillaume Vignères <guillaume.vigneres@gmail.com>
2022-03-17 18:25:10 +0100

    test task create, edite and route protected ok

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -12,55 +12,59 @@ class TaskControllerTest extends WebTestCase
     { 
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         // retrieve the test user
         $testUser = $userRepository->findOneByUsername('username_1');
-
         // simulate $testUser being logged in
         $client->loginUser($testUser);
-
         $crawler = $client->request('GET', '/tasks');
-        
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('.glyphicon-remove');
-        
+    }
+
+    public function testRouteRedirectListTaskToDo(): void
+    { 
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks');
+        $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        static::assertSelectorTextContains('button', "Se connecter");    
     }
 
     public function testRouteListTaskIsDone(): void
     { 
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         // retrieve the test user
         $testUser = $userRepository->findOneByUsername('username_1');
-
         // simulate $testUser being logged in
         $client->loginUser($testUser);
-
         $crawler = $client->request('GET', '/tasks/done');
-
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('.glyphicon-ok');
+    }
+
+    public function testRouteRedirectListTaskIsDone(): void
+    { 
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks/done');
+        $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        static::assertSelectorTextContains('button', "Se connecter");    
     }
 
     public function testCreateTask(): void
     { 
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         // retrieve the test user
         $testUser = $userRepository->findOneByUsername('username_1');
-
         // simulate $testUser being logged in
         $client->loginUser($testUser);
-
         $crawler = $client->request('GET', '/tasks/create');
-        
-        $form = $crawler->selectButton('Ajouter')->form();
 
+        $form = $crawler->selectButton('Ajouter')->form();
         $form['task[title]'] = 'Titre test';
         $form['task[content]'] = 'Contenu test';
-
         $client->submit($form);
 
         $session = $client->getContainer()->get('session');
@@ -73,28 +77,30 @@ class TaskControllerTest extends WebTestCase
         $client->followRedirect();
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
     }
 
+    public function testRouteRedirectCreate(): void
+    { 
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks/create');
+        $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        static::assertSelectorTextContains('button', "Se connecter");    
+    }
 
     public function testEditTask(): void
     { 
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         // retrieve the test user
         $testUser = $userRepository->findOneByUsername('username_1');
-
         // simulate $testUser being logged in
         $client->loginUser($testUser);
-
-        $crawler = $client->request('GET', '/tasks/create');
+        $crawler = $client->request('GET', '/tasks/1/edit');
         
-        $form = $crawler->selectButton('Ajouter')->form();
-
-        $form['task[title]'] = 'Titre test';
-        $form['task[content]'] = 'Contenu test';
-
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['task[title]'] = 'Titre mofifié test';
+        $form['task[content]'] = 'Contenu modifié test';
         $client->submit($form);
 
         $session = $client->getContainer()->get('session');
@@ -102,12 +108,20 @@ class TaskControllerTest extends WebTestCase
         
         $this->assertArrayHasKey('success', $flashes);
         $this->assertCount(1, $flashes['success']);
-        $this->assertEquals("La tâche a été bien été ajoutée.",current($flashes['success']));
+        $this->assertEquals('La tâche a bien été modifiée.',current($flashes['success']));
 
         $client->followRedirect();
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
+    }
+
+    public function testRouteRedirectEdite(): void
+    { 
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks/1/edit');
+        $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        static::assertSelectorTextContains('button', "Se connecter");    
     }
 
 }
